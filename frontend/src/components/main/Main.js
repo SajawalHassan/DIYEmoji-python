587d01c57c2d6701f9685850f947bd65d060bb61
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-24 23:53:04 +0500

    Make header

---
+++ frontend/src/components/main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+import { MicrophoneIcon } from "@heroicons/react/solid";
+
+function main() {
+  return (
+    <div className="main">
+      <div className="main_header">
+        <MicrophoneIcon className="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default main;
