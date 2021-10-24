a17eb018ea7cff295e73443bb8973d73cd7ccca4
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-25 00:03:59 +0500

    Complete main part

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Main.css";
+
 import { MicrophoneIcon } from "@heroicons/react/solid";
+import profile_pic from "./images/profilepic.jpg";
 
 function main() {
   return (
     <div className="main">
       <div className="main_header">
         <MicrophoneIcon className="icon" />
+      </div>
+
+      <div className="main_body">
+        {/* WILL CHANGE TO AUTHENTICATOR IMG */}
+        <img src={profile_pic} alt="" />
       </div>
     </div>
   );
