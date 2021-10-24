31fb466c1f680455e1484a33aa1cac025d21f254
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-24 18:49:11 +0500

    Add Database

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+# Init App
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/')
-def index():
-    return 'Hello World'
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHMY_TRACK_MODIFICATION'] = False
+# Init Database
+db = SQLAlchemy(app)
+# init marshmallow
+ma = Marshmallow(app)
 
+
+# Running the server
 if __name__ == "__main__":
     app.run(debug=True)
