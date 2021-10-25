5e2be90eb0f5f5899abbd2c0d5f58cce04ba9b41
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-25 10:40:47 +0500

    Add bottom buttons

--- frontend/src/components/bottom/Bottom.js
+++ frontend/src/components/bottom/Bottom.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Bottom.css";
 
+import { CameraIcon, ClipboardIcon, UploadIcon } from "@heroicons/react/solid";
+
 function Bottom() {
   return (
     <div className="bottom">
-      <h1>Bottom</h1>
+      <div className="bottom__center">
+        <a href="#" title="Open webcam">
+          <CameraIcon className="bottom__icon" />
+        </a>
+        <a href="#" title="Create filter">
+          <ClipboardIcon className="bottom__icon" />
+        </a>
+        <a href="#" title="Upload photo">
+          <UploadIcon className="bottom__icon" />
+        </a>
+      </div>
     </div>
   );
 }
