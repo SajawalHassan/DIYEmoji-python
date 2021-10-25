092e4789a0e7f854dd22ef5dc0cc91f9279af6e0
ShahabAthar25 <shahabathar25@gmail.com>
2021-10-25 10:05:50 +0500

    Add Route

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,14 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-import os
+from werkzeug.utils import secure_filename
 
-# Init App
+from db import db_init
+
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHMY_TRACK_MODIFICATION'] = False
-# Init Database
-db = SQLAlchemy(app)
-# init marshmallow
-ma = Marshmallow(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
+db_init(app)
 
-# Running the server
 if __name__ == "__main__":
     app.run(debug=True)
