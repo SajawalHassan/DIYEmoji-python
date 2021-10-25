f0f00ab3b7c519b00cbdb01e49bc45d11f0d04af
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-25 11:20:55 +0500

    Add date

--- frontend/src/components/bottom/Bottom.js
+++ frontend/src/components/bottom/Bottom.js
@@ -4,17 +4,25 @@ import "./Bottom.css";
 import { CameraIcon, ClipboardIcon, UploadIcon } from "@heroicons/react/solid";
 
 function Bottom() {
+  let day = new Date().getDate();
+  let month = new Date().getMonth();
+
   return (
     <div className="bottom">
+      <div className="bottom__date">
+        <p>
+          Date: 21-{month}-{day}
+        </p>
+      </div>
       <div className="bottom__center">
         <a href="#" title="Open webcam">
-          <CameraIcon className="bottom__icon" />
+          <CameraIcon className="bottom__centerIcon" />
         </a>
         <a href="#" title="Create filter">
-          <ClipboardIcon className="bottom__icon" />
+          <ClipboardIcon className="bottom__centerIcon" />
         </a>
         <a href="#" title="Upload photo">
-          <UploadIcon className="bottom__icon" />
+          <UploadIcon className="bottom__centerIcon" />
         </a>
       </div>
     </div>
